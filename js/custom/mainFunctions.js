160c0822367b6588291faf3c7c308e614a30b932
Nadji Tan <nadjitan@yahoo.com.ph>
2020-09-10 23:11:23 +0800

    Better UI and UI code

--- js/custom/mainFunctions.js
+++ js/custom/mainFunctions.js
@@ -1,5 +1,8 @@
-$(function(){
+
+/* Unused because files needs to be loaded locally */
+
+/* $(function(){
     $("#nav-placeholder").load("./common/navbar.html");
     $("#footer-placeholder").load("./common/footer.html");
-  });
-  
+}); */
+
